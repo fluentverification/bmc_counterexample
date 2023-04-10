2ea72f716a3570a16c06ef1847b91862442bdd18
Mo Ahmadi <mahmadi@usf.edu>
2023-04-10 18:43:51 -0400

    xbf first attempt

--- guided/GDFS/Main.py
+++ guided/GDFS/Main.py
@@ -1,10 +1,11 @@
 from Utils import get_min_max
-from Search_Algos import ggdfs_prob, ggdfs, post_process
+from Search_Algos import ggdfs_prob, ggdfs, post_process, XBF
 from Graph import Node, Edge, Graph
 from Model import Model
 import time
 import json
 import sys, os, re
+
 
 #read json elements
 f = open(sys.argv[1])
@@ -29,67 +30,75 @@ if not os.path.exists('./results/' + model_name):
     os.makedirs('./results/' + model_name)
 
 start_time = time.time()
-init_node = Node()
-init_node.initial_state = True
-init_node.var_values = model.get_initial_state()
-init_node.reachability_probability = 1.0
-graph = Graph()
-graph.add_node(init_node)
-graph_size = 1
-motil_thresh = 1E-10
-# enzym_thresh = 1e-
-# yeast_thresh = 1e-
-prob_thresh = motil_thresh
-flag1 = False
-flag_prob = False
-bound_changed = False
-bound = 3
-min_max = get_min_max(model, bound, target_index, target_value, max_comb)
-print(time.time() - start_time)
 
-while (True):
-    print('='*50)
-    print("bound = " + str(bound))
-    print("prob threshold = " + str(prob_thresh))
-    if bound_changed:
-        min_max = get_min_max(model, bound, target_index, target_value, max_comb)
-    flag2 = ggdfs(graph=graph, start_node=init_node, model=model, target_index=target_index, target_value=target_value, min_max=min_max)
-    #flag2 = ggdfs_trace_back(graph=graph, start_node=init_node, model=model, target_index=target_index, target_value=target_value, min_max=min_max)
-    # flag2 = ggdfs_prob(graph=graph, start_node=init_node, model=model, target_index=target_index, target_value=target_value, min_max=min_max, prob_thresh=prob_thresh, size=graph_size)
-    # graph = post_process(graph, target_index, target_value)
-    graph_size = len(graph.nodes) + len(graph.edges)
-    # flag2 = ggdfs_prob_trace_back(graph=graph, start_node=init_node, model=model, target_index=target_index, target_value=target_value, prob_thresh=prob_thresh, min_max=min_max)
-    #reduce the threshold until you find the first trace for the minimum bound. Afterwards
-    #it alternates between reducing the threshold or increasing the bound
-    # if flag1 or flag2:
-    #     flag1 = True
-    #     if bound_changed:
-    #         prob_thresh = prob_thresh / 2.0
-    #         bound_changed = False
-    #     else:
-    #         bound = bound + 1
-    #         bound_changed = True
-    # else:
-    #     prob_thresh = prob_thresh / 2.0
-    # graph = post_process(graph, target_index, target_value)
+
+XBF(model, target_index, target_value, mc_step)
+################################################################################################
+################################################################################################
+# init_node = Node()
+# init_node.initial_state = True
+# init_node.var_values = model.get_initial_state()
+# init_node.reachability_probability = 1.0
+# graph = Graph()
+# graph.add_node(init_node)
+# graph_size = 1
+# # motil_thresh = 1E-20
+# # enzym_thresh = 1e-
+# # yeast_thresh = 1e-
+# prob_thresh = 1.0
+# flag1 = False
+# flag_prob = False
+# bound_changed = False
+# # bound = 100
+# min_max = get_min_max(model, bound, target_index, target_value, max_comb)
+# print(time.time() - start_time)
+
+# while (True):
+#     print('='*50)
+#     print("bound = " + str(bound))
+#     print("prob threshold = " + str(prob_thresh))
+#     if bound_changed:
+#         min_max = get_min_max(model, bound, target_index, target_value, max_comb)
+#     # flag2 = ggdfs(graph=graph, start_node=init_node, model=model, target_index=target_index, target_value=target_value, min_max=min_max)
+#     #flag2 = ggdfs_trace_back(graph=graph, start_node=init_node, model=model, target_index=target_index, target_value=target_value, min_max=min_max)
+#     flag2 = ggdfs_prob(graph=graph, start_node=init_node, model=model, target_index=target_index, target_value=target_value, min_max=min_max, prob_thresh=prob_thresh, size=graph_size)
+#     # graph = post_process(graph, target_index, target_value)
+#     graph_size = len(graph.nodes) + len(graph.edges)
+#     #reduce the threshold until you find the first trace for the minimum bound. Afterwards
+#     #it alternates between reducing the threshold or increasing the bound
+#     if flag1 or flag2:
+#         flag1 = True
+#         if bound_changed:
+#             prob_thresh = prob_thresh / 10.0
+#             bound_changed = False
+#         else:
+#             bound = bound + 1
+#             bound_changed = True
+#     else:
+#         prob_thresh = prob_thresh / 10.0
+#     graph = post_process(graph, target_index, target_value)
     
-    bound_changed = True
-    bound = bound + 1
-    # prob_thresh = prob_thresh / 10.0
+#     # bound_changed = True
+#     # bound = bound + 1
+#     # prob_thresh = prob_thresh / 10.0
     
-    print("state-space size = " + str(len(graph.nodes) + len(graph.edges)))
-    if flag_prob or flag2:
-        # flag_prob = True
-        print("probability = " + str(graph.check_probability(model = model, file_name_prefix= model_name, prism_bin=prism_bin, csl_prop=csl_prop)))
-    else:
-        print("probability = 0")
-    current_time = time.time()
-    print("time = " + str(current_time - start_time))
-    print('='*50)
-    # quit()
-    if (not flag2) and (not flag1):
-        graph = Graph()
-        flag1 = True
-    graph.add_node(init_node)
-    if (current_time - start_time > 3600):
-        break
+#     print("state-space size = " + str(len(graph.nodes) + len(graph.edges)))
+#     if flag_prob or flag2:
+#         # flag_prob = True
+#         print("probability = " + str(graph.check_probability(model = model, file_name_prefix= model_name, prism_bin=prism_bin, csl_prop=csl_prop)))
+#     else:
+#         print("probability = 0")
+#     current_time = time.time()
+#     print("time = " + str(current_time - start_time))
+#     print('='*50)
+#     # quit()
+#     # if (not flag2) and (not flag1):
+#     #     graph = Graph()
+#     #     flag1 = True
+#     graph = Graph()
+#     graph.add_node(init_node)
+#     if (current_time - start_time > 3600):
+#         break
+
+################################################################################################
+################################################################################################
--- guided/GDFS/Search_Algos.py
+++ guided/GDFS/Search_Algos.py
@@ -1,5 +1,8 @@
 from Utils import is_target, get_reaction_rate, get_total_outgoing_rate, check_sat
 from Graph import Node, Edge, Graph
+from queue import PriorityQueue
+from heapq import heapify, heappush, heappop
+
 def construct_trace(graph, node):
     curr_node = node
     flag = True
@@ -70,54 +73,54 @@ def ggdfs(graph, start_node, model, target_index, target_value, min_max):
     
 #guided greedy DFS where all the nodes allowed by the minmax dictionary are added to search stack. 
 #All nodes added to the search stack are added to the graph.
-def ggdfs_prob(graph, start_node, model, target_index, target_value, min_max, prob_thresh, size):
-    flag = False
-    visited = {}
-    stack = [(start_node, 1.0)]
-    while stack:
-        node, prob = stack.pop()
+# def ggdfs_prob(graph, start_node, model, target_index, target_value, min_max, prob_thresh, size):
+#     flag = False
+#     visited = {}
+#     stack = [(start_node, 1.0)]
+#     while stack:
+#         node, prob = stack.pop()
         
-        if not node.var_values in visited:
-            visited[node.var_values] = node
-            if is_target(node.var_values, target_index, target_value):
-                flag = True
-                neighbors = []
-            else:
-                neighbors = []
-                for i, r in enumerate(model.get_reactions_vector()):
-                    rate = get_reaction_rate(node.var_values, model, i)
-                    total_rate = get_total_outgoing_rate(node.var_values, model)
-                    if rate > 0:
-                        dst_var_values = [None] * len(model.get_species_tuple())
-                        for j, coefficient in enumerate(r):
-                            dst_var_values[j] = node.var_values[j] + coefficient
-                        dst_var_values = tuple(dst_var_values)
-                        if check_sat(dst_var_values, min_max) and prob>prob_thresh:
-                            new_node = Node()
-                            new_node.var_values = dst_var_values
-                            new_node.reachability_probability = (float(rate/total_rate)*prob)
-                            edge = Edge()
-                            edge.src = node
-                            edge.dst = new_node
-                            edge.rate = rate
-                            edge.reaction = i
+#         if not node.var_values in visited:
+#             visited[node.var_values] = node
+#             if is_target(node.var_values, target_index, target_value):
+#                 flag = True
+#                 neighbors = []
+#             else:
+#                 neighbors = []
+#                 for i, r in enumerate(model.get_reactions_vector()):
+#                     rate = get_reaction_rate(node.var_values, model, i)
+#                     total_rate = get_total_outgoing_rate(node.var_values, model)
+#                     if rate > 0:
+#                         dst_var_values = [None] * len(model.get_species_tuple())
+#                         for j, coefficient in enumerate(r):
+#                             dst_var_values[j] = node.var_values[j] + coefficient
+#                         dst_var_values = tuple(dst_var_values)
+#                         if check_sat(dst_var_values, min_max) and prob>prob_thresh:
+#                             new_node = Node()
+#                             new_node.var_values = dst_var_values
+#                             new_node.reachability_probability = (float(rate/total_rate)*prob)
+#                             edge = Edge()
+#                             edge.src = node
+#                             edge.dst = new_node
+#                             edge.rate = rate
+#                             edge.reaction = i
 
-                            if prob>prob_thresh:
-                                if new_node.var_values in visited:
-                                    old_prob = visited[new_node.var_values].reachability_probability
-                                    if new_node.reachability_probability > old_prob:
-                                        del visited[new_node.var_values]
+#                             if prob>prob_thresh:
+#                                 if new_node.var_values in visited:
+#                                     old_prob = visited[new_node.var_values].reachability_probability
+#                                     if new_node.reachability_probability > old_prob:
+#                                         del visited[new_node.var_values]
 
-                                # if new_node.var_values in visited:
-                                #     del visited[new_node.var_values]
-                                if not graph.get_edge(edge.get_tuple())[0]:
-                                    graph.add_edge(edge)
-                                neighbors.append(new_node)
-            neighbors = sorted(neighbors, key=lambda n: n.reachability_probability, reverse = True)
-            for neighbor in neighbors:
-                if neighbor.var_values not in visited:
-                    stack.append((neighbor, new_node.reachability_probability))
-    return flag
+#                                 # if new_node.var_values in visited:
+#                                 #     del visited[new_node.var_values]
+#                                 if not graph.get_edge(edge.get_tuple())[0]:
+#                                     graph.add_edge(edge)
+#                                 neighbors.append(new_node)
+#             neighbors = sorted(neighbors, key=lambda n: n.reachability_probability, reverse = True)
+#             for neighbor in neighbors:
+#                 if neighbor.var_values not in visited:
+#                     stack.append((neighbor, new_node.reachability_probability))
+#     return flag
 
 
 
@@ -143,7 +146,7 @@ def ggdfs_prob(graph, start_node, model, target_index, target_value, min_max, pr
                         for j, coefficient in enumerate(r):
                             dst_var_values[j] = node.var_values[j] + coefficient
                         dst_var_values = tuple(dst_var_values)
-                        if check_sat(dst_var_values, min_max) and ((prob*float(rate/total_rate))>prob_thresh):
+                        if check_sat(dst_var_values, min_max) and ((prob*float(rate/total_rate))>=prob_thresh):
                             new_node = Node()
                             new_node.var_values = dst_var_values
                             edge = Edge()
@@ -160,3 +163,77 @@ def ggdfs_prob(graph, start_node, model, target_index, target_value, min_max, pr
                 for neighbor in neighbors:
                     stack.append(neighbor)
     return flag
+
+def h(reach_prob, var_value, model):
+    return reach_prob
+
+def backtrack(graph, node, model):
+    pass
+
+def XBF(model, target_index, target_value, mc_step):
+    init_node = Node()
+    init_node.initial_state = True
+    init_node.var_values = model.get_initial_state()
+    init_node.reachability_probability = 1.0 
+    # diag <-- empty graph
+    # closed <-- empty hash table
+    # open_ <-- empty priority queue (implemented using heap functions)
+    # map_ <-- mapping node's var_values to a list of size 2 containing the hueristic value and the node object
+    diag = Graph()
+    diag_size = len(diag.nodes) + len(diag.edges)
+    open_ = []
+    closed = {}
+    map_ = {}
+
+    node_list = [h(1.0, init_node.var_values, model), init_node]
+    map_[init_node.var_values] = node_list
+    heappush(open_, node_list)
+
+    while (open_):
+        h_s = heappop(open_)
+        old_h = h_s[0]
+        s = h_s[1]
+        del map_[s.var_values]
+        closed[s.var_values] = [old_h, s]
+        
+        for i, r in enumerate(model.get_reactions_vector()):
+            rate = get_reaction_rate(s.var_values, model, i)
+            total_rate = get_total_outgoing_rate(s.var_values, model)
+            if rate > 0:
+                dst_var_values = [None] * len(model.get_species_tuple())
+                for j, coefficient in enumerate(r):
+                    dst_var_values[j] = s.var_values[j] + coefficient
+                dst_var_values = tuple(dst_var_values)
+                s_prime = Node()
+                s_prime.var_values = dst_var_values
+                edge = Edge()
+                edge.src = s
+                edge.dst = s_prime
+                edge.rate = rate
+                edge.reaction = i
+                s_prime.add_in_edge(edge)
+                s_prime.reachability_probability = s.reachability_probability * float(rate/total_rate)
+
+                if is_target(s_prime.var_values, target_index, target_value):
+                    backtrack(diag, s_prime, model)
+            
+                new_h = h(s_prime.reachability_probability, s_prime.var_values, model)
+
+                if (s_prime.var_values not in map_ 
+                    and s_prime.var_values not in closed):
+                    node_list = [new_h, s_prime]
+                    heappush(open_, node_list)
+                    map_[s_prime.var_values] = node_list
+                else:
+                    if (s_prime.var_values in map_):
+                        if new_h < map_[s_prime.var_values][0]:
+                            map_[s_prime.var_values][0] = new_h
+                            heapify(open_)
+                    elif (s_prime.var_values in closed):
+                        if new_h < closed[s_prime.var_values][0]:
+                            del closed[s_prime.var_values]
+                            node_list = [new_h, s_prime]
+                            heappush(open_, node_list)
+                            map_[s_prime.var_values] = node_list
+
+
